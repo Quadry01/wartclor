09ede527b51605ceefcf08f495338d281ae77e99
Quadry Azeez Olawale <100448936+Quadry01@users.noreply.github.com>
2024-03-30 21:21:14 +0100

    all page foundation

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import Hero from "./components/Hero/page";
+import Course from "./components/Courses/page";
+import Staff from "./components/Staffs/page";
+import Footer from "./components/Footer/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world gp
-      
+    <main className=" animate-fade-up bg-light-yellow flex min-h-screen flex-col items-center justify-between ">
+     <Hero/>
+     <Course/>
+     <Staff/>
+      <Footer/>
     </main>
   );
 }
